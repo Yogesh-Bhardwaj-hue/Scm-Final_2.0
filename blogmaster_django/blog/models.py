8a9690d7e077555d01bf5638bc1757c2c89279d9
Yogesh-Bhardwaj-hue <astasaras19@gmail.com>
2025-05-06 09:51:18 +0530

    New Notifications Features

--- blogmaster_django/blog/models.py
+++ blogmaster_django/blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
@@ -16,15 +19,15 @@ class UserProfile(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(unique=True)
-    
+
     class Meta:
         verbose_name_plural = "Categories"
-        
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
-    
+
     def __str__(self):
         return self.name
 
@@ -38,20 +41,30 @@ class Blog(models.Model):
     youtube_link = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.title
 
 class BlogImage(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(upload_to='blog_images/')
-    
+
     def __str__(self):
         return f"Image for {self.blog.title}"
 
 class BulletPoint(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="bullet_points")
     text = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return self.text
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.CharField(max_length=255)
+    url = models.URLField(blank=True, null=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username}: {self.message}"
